b514ae0f72380e90424e11d6d16c3036db2d9375
Fatih Altun <altunfatih3547@gmail.com>
2024-12-24 14:33:34 +0300

    bottom navigaion bar menu icon and label colors are changed

--- lib/views/app_view.dart
+++ lib/views/app_view.dart
@@ -10,19 +10,60 @@ class AppView extends StatelessWidget {
     return Scaffold(
       appBar: _appBarWidget(),
       body: navigationShell,
-      bottomNavigationBar: NavigationBar(
-          selectedIndex: navigationShell.currentIndex,
-          onDestinationSelected: navigationShell.goBranch,
-          indicatorColor: Colors.transparent,
-          destinations: [
-            NavigationDestination(icon: Icon(Icons.face), label: 'Karakterler'),
-            NavigationDestination(
-                icon: Icon(Icons.bookmark), label: 'Favorilerim'),
-            NavigationDestination(
-                icon: Icon(Icons.location_on), label: 'Konumlar'),
-            NavigationDestination(icon: Icon(Icons.menu), label: 'Bölümler'),
-          ]),
+      bottomNavigationBar: NavigationBarTheme(
+        data: NavigationBarThemeData(
+            labelTextStyle: WidgetStateTextStyle.resolveWith(
+          (states) {
+            if (states.contains(WidgetState.selected)) {
+              return TextStyle(color: Theme.of(context).colorScheme.primary);
+            } else {
+              return TextStyle(color: Theme.of(context).colorScheme.tertiary);
+            }
+          },
+        )),
+        child: NavigationBar(
+            selectedIndex: navigationShell.currentIndex,
+            onDestinationSelected: navigationShell.goBranch,
+            indicatorColor: Colors.transparent,
+            destinations: [
+              _menuItem(context,
+                  index: 0,
+                  currentIndex: navigationShell.currentIndex,
+                  label: 'Karakterler',
+                  icon: Icons.face),
+              _menuItem(context,
+                  index: 1,
+                  currentIndex: navigationShell.currentIndex,
+                  label: 'Favorilerim',
+                  icon: Icons.bookmark),
+              _menuItem(context,
+                  index: 2,
+                  currentIndex: navigationShell.currentIndex,
+                  label: 'Konumlar',
+                  icon: Icons.location_on),
+              _menuItem(context,
+                  index: 3,
+                  currentIndex: navigationShell.currentIndex,
+                  label: 'Bölümler',
+                  icon: Icons.menu),
+            ]),
+      ),
     );
+  }
+
+  Widget _menuItem(BuildContext context,
+      {required int index,
+      required int currentIndex,
+      required String label,
+      required IconData icon}) {
+    return NavigationDestination(
+        icon: Icon(
+          icon,
+          color: currentIndex == index
+              ? Theme.of(context).colorScheme.primary
+              : Theme.of(context).colorScheme.tertiary,
+        ),
+        label: label);
   }
 
   AppBar _appBarWidget() {
